80bbad6f986f0d45b07db578b031849a786f2e21
td512 <td512@td512.io>
2020-03-29 20:02:49 +1300

    expands regex patterns
    Expands patterns to acceptable characters for Windows

--- app.rb
+++ app.rb
@@ -8,7 +8,8 @@ api_key = '' # API Key from https://www.steamgriddb.com/profile/preferences
 
 steam_applist = RestClient.get('https://api.steampowered.com/ISteamApps/GetAppList/v2/')
 decoded_applist = steam_applist.json
-
+total_apps = decoded_applist.applist.apps.count
+counter = 1
 decoded_applist.applist.apps.each do |app|
   grid_id = 0
   puts "Processing #{app.name}, AppID: #{app.appid}"
@@ -18,6 +19,8 @@ decoded_applist.applist.apps.each do |app|
     grid_id = res.body.json.data.id
   rescue RestClient::NotFound
     puts "No games found for #{app.name}, skipping"
+    puts "Processed #{counter}/#{total_apps}, #{total_apps - counter} to go"
+    counter += 1
     puts ""
     next
   rescue RestClient::Unauthorized
@@ -31,9 +34,9 @@ decoded_applist.applist.apps.each do |app|
     puts "No grids found for #{app.name}" if res.body.json.data.empty?
     res.body.json.data.each do |grid|
       open(grid.url) do |image|
-        FileUtils.mkdir_p("output/#{app.name.gsub(/\\,\/,\:,\*,\?,\",\<,\>,\|/, '-')} (#{app.appid})/grids")
-        File.open("output/#{app.name.gsub(/\\,\/,\:,\*,\?,\",\<,\>,\|/, '-')} (#{app.appid})/grids/#{grid.url.split('/').last}", 'wb') { |f| f.write(image.read) }
-        puts "Saved grid #{grid.id} to output/#{app.name.gsub(/\\,\/,\:,\*,\?,\",\<,\>,\|/, '-')} (#{app.appid})/grids/#{grid.url.split('/').last}"
+        FileUtils.mkdir_p("output/#{app.name.gsub(/[^0-9A-Za-z.\-\ \(\)\,\.\'\_\[\]\;\'\!\@\#\$\%\^\&\+\=\-]/, '-')} (#{app.appid})/grids")
+        File.open("output/#{app.name.gsub(/[^0-9A-Za-z.\-\ \(\)\,\.\'\_\[\]\;\'\!\@\#\$\%\^\&\+\=\-]/, '-')} (#{app.appid})/grids/#{grid.url.split('/').last}", 'wb') { |f| f.write(image.read) }
+        puts "Saved grid #{grid.id} to output/#{app.name.gsub(/[^0-9A-Za-z.\-\ \(\)\,\.\'\_\[\]\;\'\!\@\#\$\%\^\&\+\=\-]/, '-')} (#{app.appid})/grids/#{grid.url.split('/').last}"
       end
     end
   rescue RestClient::NotFound
@@ -46,14 +49,14 @@ decoded_applist.applist.apps.each do |app|
     puts "No heroes found for #{app.name}" if res.body.json.data.empty?
     res.body.json.data.each do |grid|
       open(grid.url) do |image|
-        FileUtils.mkdir_p("output/#{app.name.gsub(/\\,\/,\:,\*,\?,\",\<,\>,\|/, '-')} (#{app.appid})/heroes")
-        File.open("output/#{app.name.gsub(/\\,\/,\:,\*,\?,\",\<,\>,\|/, '-')} (#{app.appid})/heroes/#{grid.url.split('/').last}", 'wb') { |f| f.write(image.read) }
-        puts "Saved hero #{grid.id} to output/#{app.name.gsub(/\\,\/,\:,\*,\?,\",\<,\>,\|/, '-')} (#{app.appid})/grids/#{grid.url.split('/').last}"
+        FileUtils.mkdir_p("output/#{app.name.gsub(/[^0-9A-Za-z.\-\ \(\)\,\.\'\_\[\]\;\'\!\@\#\$\%\^\&\+\=\-]/, '-')} (#{app.appid})/heroes")
+        File.open("output/#{app.name.gsub(/[^0-9A-Za-z.\-\ \(\)\,\.\'\_\[\]\;\'\!\@\#\$\%\^\&\+\=\-]/, '-')} (#{app.appid})/heroes/#{grid.url.split('/').last}", 'wb') { |f| f.write(image.read) }
+        puts "Saved hero #{grid.id} to output/#{app.name.gsub(/[^0-9A-Za-z.\-\ \(\)\,\.\'\_\[\]\;\'\!\@\#\$\%\^\&\+\=\-]/, '-')} (#{app.appid})/grids/#{grid.url.split('/').last}"
 
       end
     end
   rescue RestClient::NotFound
-    puts "No logos found for #{app.name}"
+    puts "No heroes found for #{app.name}"
   end
 
   begin
@@ -62,14 +65,16 @@ decoded_applist.applist.apps.each do |app|
     puts "No logos found for #{app.name}" if res.body.json.data.empty?
     res.body.json.data.each do |grid|
       open(grid.url) do |image|
-        FileUtils.mkdir_p("output/#{app.name.gsub(/\\,\/,\:,\*,\?,\",\<,\>,\|/, '-')} (#{app.appid})/logos")
-        File.open("output/#{app.name.gsub(/\\,\/,\:,\*,\?,\",\<,\>,\|/, '-')} (#{app.appid})/logos/#{grid.url.split('/').last}", 'wb') { |f| f.write(image.read) }
-        puts "Saved logo #{grid.id} to output/#{app.name.gsub(/\\,\/,\:,\*,\?,\",\<,\>,\|/, '-')} (#{app.appid})/grids/#{grid.url.split('/').last}"
+        FileUtils.mkdir_p("output/#{app.name.gsub(/[^0-9A-Za-z.\-\ \(\)\,\.\'\_\[\]\;\'\!\@\#\$\%\^\&\+\=\-]/, '-')} (#{app.appid})/logos")
+        File.open("output/#{app.name.gsub(/[^0-9A-Za-z.\-\ \(\)\,\.\'\_\[\]\;\'\!\@\#\$\%\^\&\+\=\-]/, '-')} (#{app.appid})/logos/#{grid.url.split('/').last}", 'wb') { |f| f.write(image.read) }
+        puts "Saved logo #{grid.id} to output/#{app.name.gsub(/[^0-9A-Za-z.\-\ \(\)\,\.\'\_\[\]\;\'\!\@\#\$\%\^\&\+\=\-]/, '-')} (#{app.appid})/grids/#{grid.url.split('/').last}"
       end
     end
   rescue RestClient::NotFound
     puts "No logos found for #{app.name}"
     puts ""
   end
+  puts "Processed #{counter}/#{total_apps}, #{total_apps - counter} to go"
+  counter += 1
   puts ""
 end
